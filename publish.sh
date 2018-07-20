85825f23f90cab10041547a5503b7cb4d0e4233f
Jannis R <mail@jannisr.de>
2018-07-20 21:44:40 +0200

    fix build :green_heart:

--- publish.sh
+++ publish.sh
@@ -6,5 +6,7 @@ package='pax-static-'$1
 cp bin/pax packages/$package/pax
 
 cd packages/$package
+npm i semver --no-save
+npm version --no-git-tag-version --force $(../../node_modules/.bin/semver -i minor $(npm info $package version))
 echo "//registry.npmjs.org/:_authToken=$NPM_KEY" >~/.npmrc
 npm publish
